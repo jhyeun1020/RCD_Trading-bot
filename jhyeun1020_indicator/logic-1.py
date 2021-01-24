31f71d01651395ba6f334399c8d78e896756862b
Junghyun <jhyeun1020@naver.com>
2021-01-24 13:51:06 +0900

    데이터 내보내기

--- jhyeun1020_indicator/logic-1.py
+++ jhyeun1020_indicator/logic-1.py
@@ -4,5 +4,10 @@ import pandas as pd
 
 binance = ccxt.binance() # ccxt 모듈에서 바이낸스 객체 생성
 ohlc = binance.fetch_ohlcv('BTC/USDT') # 날짜, 시가, 고가, 저가, 종가, 거래량을 리스트로 가져옴
-df = pd.DataFrame(ohlc) # 리스트로 가져온 ohlcv 정보를 데이터 프레임으로 전환
-df.to_excel("BTC_USDT.xlsx") # 데이터 프레임 엑셀로 내보냄
+for i in ohlc: # 모든 날짜 데이터를 데이트타임 타입에서 읽기 쉽게 변경
+    i[0] = datetime.fromtimestamp(i[0]/1000).strftime('%Y-%m-%d %H:%M:%S')
+ # 리스트로 가져온 ohlcv 정보를 데이터 프레임으로 전환
+ # 데이터 프레임의 각 열을 일기 쉽게 인덱싱
+df = pd.DataFrame(ohlc,columns=['date','open','high','low','close','volume'])
+# 엑셀 파일로 변환
+df.to_excel('BTC_USDT.xlsx')
